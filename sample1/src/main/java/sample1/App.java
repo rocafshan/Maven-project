2d4124241f9a08d9d792d7d0295185017497ffe9
Afshan <rocafshan@gmail.com>
2024-09-12 16:56:13 +0530

    message

---
+++ sample1/src/main/java/sample1/App.java
@@ -0,0 +1,17 @@
+package sample1;
+
+public class App {
+	 void A()
+	{
+		System.out.println("A");
+	}
+	 void B()
+	 {
+		 System.out.println("A"); 
+	 }
+	 void c()
+	 {
+		 System.out.println("A"); 
+	 }
+
+}
